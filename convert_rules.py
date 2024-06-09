763ea1a7d3e3383b1a1feaaec76c7431e224a1e0
KeQing Ling <147781426+QingRex@users.noreply.github.com>
2024-06-09 21:45:13 +0800

    Update convert_rules.py

--- convert_rules.py
+++ convert_rules.py
@@ -1,11 +1,17 @@
 import os
 import yaml
 
-# 输入文件夹和输出文件夹路径
 input_folder = 'rules'
 output_folder = 'converted_rules'
 
 os.makedirs(output_folder, exist_ok=True)
+
+# 自定义的Dumper类
+class SingleQuotedDumper(yaml.SafeDumper):
+    def represent_scalar(self, tag, value, style=None):
+        if tag == 'tag:yaml.org,2002:str' and not value.startswith("'") and not value.endswith("'"):
+            return super().represent_scalar(tag, value, style="'")
+        return super().represent_scalar(tag, value, style)
 
 for filename in os.listdir(input_folder):
     if filename.endswith('.rules'):
@@ -13,16 +19,16 @@ for filename in os.listdir(input_folder):
         
         with open(input_path, 'r') as infile:
             lines = infile.readlines()
-            # 过滤掉注释行（以 '#' 开头）和空行，并去除每行的首尾空白字符
-            # 同时为每个IP地址添加单引号
-            payload = [f"'{line.strip()}'" for line in lines if line.strip() and not line.startswith('#')]
+            payload = [line.strip() for line in lines if line.strip() and not line.startswith('#')]
 
         new_filename = f"{os.path.splitext(filename)[0]}.txt"
         output_path = os.path.join(output_folder, new_filename)
 
         with open(output_path, 'w') as outfile:
-            yaml.dump({'payload': payload}, outfile, default_flow_style=False, allow_unicode=True)
+            # 使用自定义的SingleQuotedDumper类将字典 {'payload': payload} 写入文件
+            yaml.dump({'payload': payload}, outfile, default_flow_style=False, allow_unicode=True, Dumper=SingleQuotedDumper)
 
         print(f"Converted {input_path} to {output_path}")
-        
+
 print("Rule conversion completed.")
+
