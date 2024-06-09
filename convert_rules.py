4d3007146df1e40a3ca13332566b860c035dadb9
KeQing Ling <147781426+QingRex@users.noreply.github.com>
2024-06-09 21:52:35 +0800

    Update convert_rules.py

--- convert_rules.py
+++ convert_rules.py
@@ -1,5 +1,6 @@
 import os
 import yaml
+from yaml.representer import SafeRepresenter
 
 # 输入文件夹和输出文件夹路径
 input_folder = 'rules'
@@ -7,6 +8,15 @@ output_folder = 'converted_rules'
 
 # 确保输出文件夹存在，如果不存在则创建
 os.makedirs(output_folder, exist_ok=True)
+
+# 自定义的字符串类型，确保YAML序列化时使用单引号
+class SingleQuotedScalarString(str):
+    pass
+
+def single_quoted_scalarstring_representer(dumper, data):
+    return dumper.represent_scalar('tag:yaml.org,2002:str', data, style="'")
+
+yaml.add_representer(SingleQuotedScalarString, single_quoted_scalarstring_representer)
 
 # 遍历输入文件夹中的所有文件
 for filename in os.listdir(input_folder):
@@ -20,7 +30,7 @@ for filename in os.listdir(input_folder):
             lines = infile.readlines()
             # 过滤掉注释行（以 '#' 开头）和空行，并去除每行的首尾空白字符
             # 同时为每个IP地址添加单引号
-            payload = [f"'{line.strip()}'" for line in lines if line.strip() and not line.startswith('#')]
+            payload = [SingleQuotedScalarString(line.strip()) for line in lines if line.strip() and not line.startswith('#')]
 
         # 创建新的文件名，替换扩展名为 .txt
         new_filename = f"{os.path.splitext(filename)[0]}.txt"
@@ -35,6 +45,3 @@ for filename in os.listdir(input_folder):
 
 # 输出转换完成的信息到控制台
 print("Rule conversion completed.")
-
-
-
